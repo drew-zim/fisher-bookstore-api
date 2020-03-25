489f876f0b35e7e2727814a8d214efe247dbc235
Drew Zimmerman <zimmerman.636@osu.edu>
2020-03-25 16:25:26 -0400

    authors-controller

--- Services/AuthorRepository.cs
+++ Services/AuthorRepository.cs
@@ -1,8 +1,13 @@
+using System;
+
 namespace Fisher.Bookstore.Services
 {
 
     public class AuthorsRepository
     {
-
+             internal static bool AuthorExists(int authorId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
